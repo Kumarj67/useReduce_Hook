4d61a8801272a274a8fa930f4aa5cccbd6f9ec73
Jeetesh <jeeteshkumar45@gmail.com>
2023-01-27 12:49:10 +0530

    first Commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import "./App.css";
+import { useReducer } from "react";
+import Header from "./Components/Header";
+
+const reducer = (state, action) => {
+  console.log("state:", state);
+  console.log("action:", action);
+  if (action.type === "INC") {
+    return state + 2;
+  } else if (action.type === "DEC") {
+    return state - 2;
+  } else if (action.type === "MUL") {
+    return state * 2;
+  }
+  return state;
+};
+
+function App() {
+  const [state, dispatch] = useReducer(reducer, 0);
+
+  return (
+    <>
+      <Header />
+      <h1>{state}</h1>
+      <button onClick={() => dispatch({ type: "INC" })}>Increment</button>
+      <button onClick={() => dispatch({ type: "DEC" })}>Decrement</button>
+      <button onClick={() => dispatch({ type: "MUL" })}>Multiply</button>
+    </>
+  );
+}
+
+export default App;
